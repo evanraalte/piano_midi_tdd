cb12c335311d8aef7997f2a1a49bb3401f5b5ab6
Erik van Raalte <erik.vanraalte94@gmail.com>
2023-10-21 19:34:10 +0200

    add tests white key detection

--- piano_midi_tdd/frame.py
+++ piano_midi_tdd/frame.py
@@ -5,13 +5,41 @@ import numpy as np
 WHITE_KEY_NUM = 52
 
 
+def find_adjacent_groups(numbers: list[int], threshold: int) -> list[tuple[int, int]]:
+    if threshold < 0:
+        raise ValueError("Threshold must be non-negative")
+
+    if any(num < 0 for num in numbers):
+        raise ValueError("Numbers in the array must be non-negative")
+
+    groups = []
+    current_group_start = None
+    last_number = None
+    for number in numbers:
+        if last_number is None:
+            current_group_start = number  # initial case
+        elif number == last_number + 1:
+            pass  # same group
+        elif number > last_number + 1:  # detect a gap
+            if last_number - current_group_start >= threshold - 1:
+                groups.append((current_group_start, last_number))  # add group to groups
+            current_group_start = number
+        last_number = number
+    if (
+        current_group_start is not None and last_number is not None
+    ):  # make sure both exist
+        if last_number - current_group_start >= threshold - 1:
+            groups.append((current_group_start, last_number))  # add last group
+    return groups
+
+
 def detect_white_keys(
     frame: np.ndarray[np.uint8],
     scan_line: int = 0,
     bg_color: tuple[np.uint8, np.uint8, np.uint8] = (0, 0, 0),
 ) -> list[int]:
     # Initialize a list to store non-background rows
-    key_width = len(frame[0]) / WHITE_KEY_NUM
+    key_width_px = len(frame[0]) / WHITE_KEY_NUM
     non_background_rows = []
     threshold = 10
     # Iterate through the rows of the image
@@ -22,7 +50,12 @@ def detect_white_keys(
         # Check if any channel difference exceeds the threshold
         if np.any(color_difference > threshold):
             non_background_rows.append(idx)
-    idx_min = min(non_background_rows)
-    idx_max = max(non_background_rows)
-    key = math.ceil(idx_min / key_width)
-    return [key]
+
+    groups = find_adjacent_groups(non_background_rows, 5)
+    keys = []
+    for start_px, end_px in groups:
+        key_start = math.ceil(start_px / key_width_px)
+        group_size_px = end_px - start_px
+        num_keys = round(group_size_px / key_width_px)
+        keys += list(range(key_start, key_start + num_keys))
+    return keys
--- tests/test_frame_detect.py
+++ tests/test_frame_detect.py
@@ -1,10 +1,29 @@
 import cv2
 import numpy as np
+import pytest
 from hypothesis import given
 from hypothesis import strategies as st
 
 from piano_midi_tdd.frame import detect_white_keys
+from piano_midi_tdd.frame import find_adjacent_groups
 from piano_midi_tdd.frame import WHITE_KEY_NUM
+
+
+@st.composite
+def integer_list_strategy(draw):  # type: ignore
+    # Determine the length of the list
+    list_length = draw(st.integers(min_value=0, max_value=WHITE_KEY_NUM - 1))
+
+    # Generate a list of integers within the specified range
+    integer_list = draw(
+        st.lists(
+            st.integers(min_value=0, max_value=WHITE_KEY_NUM - 1),
+            min_size=list_length,
+            max_size=list_length,
+        )
+    )
+
+    return integer_list
 
 
 def generate_white_keys_in_frame(
@@ -43,3 +62,46 @@ def test_can_detect_a_white_key_press_in_frame(key_num: int) -> None:
     bg_color = (43, 42, 43)
     frame = generate_white_keys_in_frame(pressed_keys=[key_num], bg_color=bg_color)
     assert detect_white_keys(frame, bg_color=bg_color) == [key_num]
+
+
+@given(integer_list_strategy())
+def test_can_detect_multiple_white_key_press_in_frame(key_nums: list[int]) -> None:
+    bg_color = (43, 42, 43)
+    frame = generate_white_keys_in_frame(pressed_keys=key_nums, bg_color=bg_color)
+    detected_keys = detect_white_keys(frame, bg_color=bg_color)
+    if set(detected_keys) != set(key_nums):
+        print(f"Detected: {detected_keys}")
+        # cv2.imshow("",frame)
+        # cv2.waitKey(0)
+        assert False
+
+
+def test_adjacent_groups() -> None:
+    # Test when there are multiple adjacent groups
+    numbers = [1, 2, 3, 5, 6, 7, 8, 10, 11]
+    assert find_adjacent_groups(numbers, threshold=2) == [(1, 3), (5, 8), (10, 11)]
+
+    # Test with a threshold of 2
+    numbers = [1, 2, 4, 5, 9, 10]
+    assert find_adjacent_groups(numbers, threshold=2) == [(1, 2), (4, 5), (9, 10)]
+
+    # Test with a threshold of 3
+    numbers = [1, 2, 5, 6, 10, 11, 12]
+    assert find_adjacent_groups(numbers, threshold=3) == [(10, 12)]
+
+    # Test when the list contains a single number
+    numbers = [1]
+    assert find_adjacent_groups(numbers, threshold=1) == [(1, 1)]
+
+    # Test with negative numbers (should raise a ValueError)
+    with pytest.raises(ValueError):
+        numbers = [-2, -1, 0, 1, 3, 4]
+        find_adjacent_groups(numbers, threshold=1)
+
+    # Test with negative threshold (should raise a ValueError)
+    with pytest.raises(ValueError):
+        numbers = [1, 3, 5, 9, 10, 11, 15, 20]
+        find_adjacent_groups(numbers, threshold=-2)
+
+    numbers = [0, 1, 2]
+    assert find_adjacent_groups(numbers, 2) == [(0, 2)]
